23dff086b3f44596593ef542d3687b3aae46bb40
AlinKinanti <41261096+AlinKinanti@users.noreply.github.com>
2019-12-11 13:37:25 +0700

    Ada perubahan di salah satu line
    Mohon di lihat

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
     <b>Version</b> 1.0.0
+    Ini adalah yang diubah
   </div>
   <strong>Copyright &copy; 2019 <a href="#">PKL FILKOM</a></strong>
 </footer>
